165ee2a584010320e3b61783f04dc0c67c256088
Daniel Samuel <danielsamuel@Daniels-MacBook-Pro-2.local>
2025-01-27 09:22:03 +0200

    worked on the route

--- app.py
+++ app.py
@@ -162,4 +162,4 @@ def food_detect():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=443, debug=True)
+    app.run(host="0.0.0.0", port=443, ssl_context=("selfsigned.crt", "selfsigned.key"))
